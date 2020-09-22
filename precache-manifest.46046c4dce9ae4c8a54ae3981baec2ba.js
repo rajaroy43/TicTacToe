119881429eed162a8ce8d2763f556be44fe0e8ac
rajaroy43 <58074523+rajaroy43@users.noreply.github.com>
2020-09-23 01:28:46 +0530

    initial commiit

---
+++ precache-manifest.46046c4dce9ae4c8a54ae3981baec2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96db9d8a9ae48b2ea314d7e731cffed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e2216e1a6421f5478d6",
+    "url": "/static/css/main.30377224.chunk.css"
+  },
+  {
+    "revision": "c80c6e4facea854ab98e",
+    "url": "/static/js/2.7639b3d2.chunk.js"
+  },
+  {
+    "revision": "080ae872e1244935c80a18351e363057",
+    "url": "/static/js/2.7639b3d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2216e1a6421f5478d6",
+    "url": "/static/js/main.f6c8308a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
